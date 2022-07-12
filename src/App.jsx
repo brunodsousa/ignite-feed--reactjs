444b6bf4b7144982c5dc2dba5aebbd07ea8fa6e5
Bruno Sousa <brunodsousa@outlook.com>
2022-07-11 22:01:23 -0300

    post and comment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Post } from "./components/Post/Post";
 
 import styles from "./App.module.css";
 import "./global.css";
@@ -10,7 +11,10 @@ export function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>Main</main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
