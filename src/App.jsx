7f041a5d1b136cdee0ad11cfd00af4494025c2a6
Bruno Sousa <brunodsousa@outlook.com>
2022-07-12 08:49:07 -0300

    props - post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,45 @@ import { Post } from "./components/Post/Post";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/brunodsousa.png",
+      name: "Bruno Sousa",
+      role: "Software Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-07-05T06:24:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/taiama.png",
+      name: "Taiãma América",
+      role: "Designer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala pessoal 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻",
+      },
+      { type: "link", content: "devonlane.design" },
+    ],
+    publishedAt: new Date("2022-07-12T08:36:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -12,8 +51,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
